1cee599fc1ce2244e956af8251194f9bd9be877f
riddhi365 <riddhi.singh2005@gmail.com>
2025-12-18 16:43:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let slides = document.querySelectorAll(".slide");
 let current = 0;
 
 setInterval(() => {
-  slides[current].classList.remove("active");
+  slides[current].classList.remove("active"); 
   current = (current + 1) % slides.length;
   slides[current].classList.add("active");
 }, 3500);
